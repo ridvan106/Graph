937a2577b813ac98687e4550031be32558c39ddf
Rıdvan <rdemirci@gtu.edu.tr>
2018-03-10 02:06:51 +0300

    init

---
+++ src/main/java/graph.java
@@ -0,0 +1,15 @@
+/**
+ * Created by oem on 3/10/18.
+ */
+public class graph {
+    private String deneme;
+
+    public graph(String deneme) {
+        this.deneme = deneme;
+    }
+
+    public String getDeneme() {
+        return deneme;
+    }
+}
+
---
+++ src/test/java/graphTest.java
@@ -0,0 +1,12 @@
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Created by oem on 3/10/18.
+ */
+public class graphTest extends TestCase {
+    public void testGraph(){
+        graph g = new graph("osman");
+        Assert.assertEquals(true,g.getDeneme().equals("osman"));
+    }
+}
